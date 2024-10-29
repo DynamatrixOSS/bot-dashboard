c24094299066461cd9d491925dad72ac5344ca00
Joshua Slui <joshua@dynamatrix.io>
2024-10-29 15:18:20 +0100

    Add fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://kit.fontawesome.com/b4cb0fdbd3.js" crossorigin="anonymous"></script>
 
         <title>{{config('app.name', 'Laravel')}}</title>
 
