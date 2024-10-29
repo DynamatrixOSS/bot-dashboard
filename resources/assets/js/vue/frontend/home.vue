611e4042fda063803bd3ef37ae3a534035ff1c00
Joshua Slui <joshua@dynamatrix.io>
2024-10-29 15:19:06 +0100

    Add home page file

---
+++ resources/assets/js/vue/frontend/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 class="text text-red">Example showcasing</h1>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Home",
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "../../../styles/sass/frontend/home/main.scss";
+</style>
