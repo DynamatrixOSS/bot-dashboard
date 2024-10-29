e325e45f079aac5928680db57ef598d77781d73c
Joshua Slui <joshua@dynamatrix.io>
2024-10-29 15:19:38 +0100

    Add privacy and terms files

---
+++ resources/assets/js/vue/frontend/privacy.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="text text-red">Example showcasing</h1>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Privacy",
+}
+
+</script>
+
+<style scoped lang="scss">
+</style>
