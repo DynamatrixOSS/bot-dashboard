6d11af338e6e7846e5d6b969ee9581b83d391661
Joshua Slui <joshua@dynamatrix.io>
2024-10-29 15:18:29 +0100

    Add navbar to App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,17 @@
 <template>
+    <ComputerNavbar></ComputerNavbar>
     <div>
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+import ComputerNavbar from "../assets/js/vue/components/navbar/computer.vue"
+
+export default {
+    name: "app",
+    components: {
+        ComputerNavbar,
+    },
+};
+</script>
