4ed0f69fec841c2f9ebc8131f437da3455e55a2c
Joshua Slui <joshua@dynamatrix.io>
2024-12-02 13:11:04 +0100

    rename navbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,12 @@
 <template>
     <ComputerNavbar></ComputerNavbar>
-    <div>
+    <div class="body">
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-import ComputerNavbar from "../assets/js/vue/components/navbar/computer.vue"
+import ComputerNavbar from "../assets/js/vue/components/navbar/navbar.vue"
 
 export default {
     name: "app",
