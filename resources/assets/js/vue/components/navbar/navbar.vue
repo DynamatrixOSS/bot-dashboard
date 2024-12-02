50f2b6c125776be6d92e87533e99f27aa3d78ea4
Joshua Slui <joshua@dynamatrix.io>
2024-12-02 13:12:03 +0100

    Mobile compatible navbar

---
+++ resources/assets/js/vue/components/navbar/navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="navbar">
+        <div class="navbar__mobile">
+            <router-link :to="{name: 'Home'}">
+                <div class="navbar__logo">
+                    <p class="navbar__logo--big">Rebel</p>
+                    <p class="navbar__logo--small">By Dynamatrix</p>
+                </div>
+            </router-link>
+            <i class="fa fa-bars navbar__burger" @click="navbarIsOpen = !navbarIsOpen"></i>
+        </div>
+        <i class="navbar__separator fa fa-angle-right"></i>
+        <a class="navbar__link" v-if="isMobile && navbarIsOpen || !isMobile" href="https://discord.gg/Z8KGvGWZcY" target="_blank">Support</a>
+        <router-link class="navbar__link" v-if="isMobile && navbarIsOpen || !isMobile" :to="{name: 'Privacy'}">
+            Privacy
+        </router-link>
+        <router-link class="navbar__link" v-if="isMobile && navbarIsOpen || !isMobile" :to="{name: 'Terms'}">
+            Terms
+        </router-link>
+
+
+        <div class="navbar__buttons" v-if="isMobile && navbarIsOpen || !isMobile">
+            <a class="button button--secondary" href="https://dynamatrix.io">
+                <i class="fas fa-globe"></i>
+                Dynamatrix
+            </a>
+            <button class="button button--primary">
+                <i class="fab fa-discord"></i>
+                Sign in
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "ComputerNavbar",
+    data() {
+        return {
+            navbarIsOpen: false,
+            isMobile: window.innerWidth <= 767
+        }
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "../../../../styles/sass/partials/navbar/navbar";
+@import "../../../../styles/sass/components/buttons";
+</style>
