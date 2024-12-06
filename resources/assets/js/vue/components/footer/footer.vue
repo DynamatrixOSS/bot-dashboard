d872eae5377d34e7edf87072e77610630c4a934e
Joshua Slui <joshua@dynamatrix.io>
2024-12-06 16:22:23 +0100

    Add footer

---
+++ resources/assets/js/vue/components/footer/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="footer">
+        <h1 class="brandText">Copyright {{new Date().getFullYear()}} - <a href="https://dynamatrix.io" target="_blank">Dynamatrix B.V.</a></h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Footer",
+}
+
+</script>
+
+<style scoped lang="scss">
+@import "../../../../styles/sass/partials/footer/footer";
+</style>
