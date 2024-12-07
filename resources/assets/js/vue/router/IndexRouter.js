1e4aa3cb1d0f4f010efb3c94c4c66b751cb1ddf6
Joshua Slui <joshua@dynamatrix.io>
2024-12-07 01:58:41 +0100

    Import pages instead, comment and add auth child route

--- resources/assets/js/vue/router/IndexRouter.js
+++ resources/assets/js/vue/router/IndexRouter.js
@@ -1,20 +1,41 @@
+// Vital imports for Vue router
 import { createRouter, createWebHistory } from "vue-router";
+
+// Imports for public frontend pages
+import homePage from "../frontend/home.vue";
+import privacyPage from "../frontend/privacy.vue";
+import termsPage from "../frontend/terms.vue";
+
+// Imports for authentication pages
+import authCallback from "../authentication/callback.vue";
+
+const authRoutes = [
+    {
+        path: "callback",
+        name: "authCallback",
+        component: () => authCallback
+    }
+]
 
 const routes = [
     {
         path: "/",
         name: "Home",
-        component: () => import("../frontend/home.vue")
+        component: () => homePage
     },
     {
         path: "/privacy",
         name: "Privacy",
-        component: () => import("../frontend/privacy.vue")
+        component: () => privacyPage
     },
     {
         path: "/terms",
         name: "Terms",
-        component: () => import("../frontend/terms.vue")
+        component: () => termsPage
+    },
+    {
+        path: "/auth",
+        children: authRoutes,
     }
 ];
 
