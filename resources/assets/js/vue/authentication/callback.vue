d4cc730526c154b2a1bc9623d40ba44ac030d738
Joshua Slui <joshua@dynamatrix.io>
2024-12-07 01:58:20 +0100

    Very basic callback page

---
+++ resources/assets/js/vue/authentication/callback.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="text text-red">Attempting to authenticate you, please wait...</h1>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "authCallback",
+}
+
+</script>
+
+<style scoped lang="scss">
+</style>
