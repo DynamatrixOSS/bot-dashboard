c4515b5338fe8732531c4321d2438de87063d79d
Joshua Slui <joshua@dynamatrix.io>
2024-12-14 13:04:20 +0100

    Delete footer

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,18 +2,15 @@
     <Navbar></Navbar>
     <div class="routecontainer">
         <router-view></router-view>
-        <Footer></Footer>
     </div>
 </template>
 
 <script>
 import Navbar from "../assets/js/vue/components/navbar/navbar.vue"
-import Footer from "../assets/js/vue/components/footer/footer.vue"
 export default {
     name: "app",
     components: {
         Navbar,
-        Footer,
     },
 };
 </script>
