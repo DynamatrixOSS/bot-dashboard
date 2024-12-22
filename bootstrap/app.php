6f6d79309948bb8b4b578eca57e307791c78c92b
Joshua Slui <joshua@dynamatrix.io>
2024-12-22 21:18:50 +0100

    Add statefulApi to middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
